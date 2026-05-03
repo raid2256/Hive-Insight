ad5d16bca2a417eaf810e55f57b2940ae5b4fbfe
Raid225 <95387935+raid2256@users.noreply.github.com>
2026-05-04 08:49:29 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1048,7 +1048,8 @@ function enforceSessionAccess(loadedIGN) {
 
   const account = JSON.parse(linked);
 
-  if (account.ign === loadedIGN) {
+  if (account.ign.toLowerCase() === loadedIGN.toLowerCase())
+
     if (controls) controls.style.display = "flex";
   } else {
     if (controls) controls.style.display = "none";
